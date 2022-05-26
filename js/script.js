eabc3165463b16ccc49855af7ad91321e7cdd7d6
Diego Viana <diegovianaf7@gmail.com>
2022-05-26 13:42:04 -0300

    refactor using eslint

--- js/script.js
+++ js/script.js
@@ -5,22 +5,24 @@ const navLinks = document.querySelectorAll('.nav-link')
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault()
-  
+
   navMenu.classList.toggle('active')
 
   const active = navMenu.classList.contains('active')
   event.currentTarget.setAttribute('aria-expanded', active)
 
-  active ? event.currentTarget.setAttribute('aria-label', 'Close Menu') : event.currentTarget.setAttribute('aria-label', 'Open Menu')
-
-  // Disable scrolling in the background when the hamburger menu is open
-  active ? body.classList.add('fixed-position') : body.classList.remove('fixed-position')
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Close Menu')
+    body.classList.add('fixed-position')
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Open Menu')
+    body.classList.remove('fixed-position')
+  }
 }
 
 mobileButton.addEventListener('click', toggleMenu)
 mobileButton.addEventListener('touchstart', toggleMenu)
 
-// Close hamburger menu when we click a link.
 function closeMenu() {
   navMenu.classList.remove('active')
   body.classList.remove('fixed-position')
