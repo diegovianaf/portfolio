d47c06a144948cc1529d3bf6a44f632075411d01
Diego Viana <diegovianaf7@gmail.com>
2022-05-26 11:27:44 -0300

    fix position of body when the hamburger menu is open to disable scrolling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const mobileButton = document.querySelector('.mobile-btn')
 const navMenu = document.querySelector('.navbar')
+const body = document.querySelector('body')
+const navLinks = document.querySelectorAll('.nav-link')
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault()
@@ -10,16 +12,19 @@ function toggleMenu(event) {
   event.currentTarget.setAttribute('aria-expanded', active)
 
   active ? event.currentTarget.setAttribute('aria-label', 'Close Menu') : event.currentTarget.setAttribute('aria-label', 'Open Menu')
+
+  // Disable scrolling in the background when the hamburger menu is open
+  active ? body.classList.add('fixed-position') : body.classList.remove('fixed-position')
 }
 
 mobileButton.addEventListener('click', toggleMenu)
 mobileButton.addEventListener('touchstart', toggleMenu)
 
 // Close hamburger menu when we click a link.
-const navLinks = document.querySelectorAll('.nav-link')
-
 function closeMenu() {
   navMenu.classList.remove('active')
+  body.classList.remove('fixed-position')
 }
 
 navLinks.forEach((navLink) => navLink.addEventListener('click', closeMenu))
+navLinks.forEach((navLink) => navLink.addEventListener('touchstart', closeMenu))
